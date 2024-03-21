fef91af20b9f27a7bb442d8fc68860dfc8c30190
anatmb <anatmb@hotmail.com>
2024-03-21 15:10:07 -0300

    Estilo de home/footer/header

--- FrontEnd-ShoeKingdom/src/Componentes/home/Home.jsx
+++ FrontEnd-ShoeKingdom/src/Componentes/home/Home.jsx
@@ -4,24 +4,26 @@ import React from 'react'
 import imagen2 from '/assets/foto3.jpg';
 import Product from '../product/Product';
 import { NavLink } from 'react-router-dom';
+import DATA from '../../Data';
 
 const Home = () => {
   return (
     <>
-      <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item active fondo-slide1">
-            <div class="container ">
-              <div class="row p-5">
-                <div class="mx-auto col-md-8 col-lg-6 order-lg-last">
-                  <img class="img-fluid" src="./assets/img/slide1.png" alt="" />
+      <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
+        <div className="carousel-inner">
+          <div className="carousel-item active fondo-slide1">
+            <div className="container ">
+              <div className="row p-5">
+                <div className="mx-auto col-md-8 col-lg-6 order-lg-last">
+                  <img className="img-fluid" src="./assets/img/slide1.png" alt="" />
                 </div>
-                <div class="col-lg-6 mb-0 d-flex align-items-center">
-                  <div class="text-align-left align-self-center">
-                    <h1 className="titulo text-primary"><b>Zapatillas</b></h1>
-                    <h3 class="h2">Encuentra las mejores zapatillas</h3>
+                <div className="col-lg-6 mb-0 d-flex align-items-left">
+                  <div>
+                    <h1 className="titulo text-primary"><b> ShoeKingdom </b></h1>
+                    {/* <h3 className="h2">Encuentra las mejores zapatillas</h3> */}
+                    <hr />
                     <p>
-                      Y una amplia variedad de marcas y estilos de zapatillas
+                      Tenemos una amplia y variedad de marcas y estilos de zapatillas
                       para todas las ocasiones. Ya sea que busques zapatillas deportivas,
                       casuales o elegantes, tenemos lo que necesitas.
                     </p>
@@ -31,17 +33,16 @@ const Home = () => {
               </div>
             </div>
           </div>
-          <div class="carousel-item  fondo">
-            <div class="container">
-              <div class="row px-2">
-                <div class="mx-auto col-md-8 col-lg-6 ">
-                  {/* <img class="img-fluid" src="./assets/img/foto.png" alt="" /> */}
+          <div className="carousel-item  fondo">
+            <div className="container">
+              <div className="row px-2">
+                <div className="mx-auto col-md-8 col-lg-6 ">
                 </div>
-                <div class="col-lg-5 mb-0 d-flex align-items-center px-5 order-lg-last">
+                <div className="col-lg-5 mb-0 d-flex align-items-center px-5 order-lg-last">
                   <div>
-                    <h1 class="titulo2"><b>Sale</b></h1>
+                    <h1 className="titulo2"><b>Sale</b></h1>
                     <p className='subtitulo'><b>50% OF</b></p>
-                    <p class="parrafo">
+                    <p className="parrafo">
                        HASTA  AGOTAR  STOCK
                     </p>
                     <br/>
@@ -51,11 +52,6 @@ const Home = () => {
               </div>
             </div>
           </div>
-         
-         
-
-
-
           <div class="carousel-item ">
             <div class="container ">
               <div class="row p-5">
@@ -87,10 +83,7 @@ const Home = () => {
           <span class="visually-hidden">Next</span>
         </button>
       </div>
-
       <div>
-
-
       </div>
 
       <section class="container py-5">
@@ -99,8 +92,11 @@ const Home = () => {
             <h1 class="h1 text-primary">Ofertas del mes</h1>
             <hr />
             <p>
-              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-              deserunt mollit anim id est laborum.
+            ¡ShoeKingdom esta precios irresistibles este mes! Descubre una amplia
+               gama de estilos y colores en nuestra oferta especial. Desde deportivas hasta 
+               casuales, encuentra el par perfecto para cualquier ocasión. 
+               ¡No te pierdas esta oportunidad de renovar tu calzado con estilo
+                y calidad a un precio inigualable!
             </p>
           </div>
         </div>
@@ -108,21 +104,21 @@ const Home = () => {
 
           <div className='class="col-12 col-md-4 p-2 mt-2'>
             <div class="card h-100 p-3">
-              <a href="#"><img src="./assets/img/category_img_02.jpg" class="rounded-circle img-fluid border" /></a>
+              <a href="#"><img src="./assets/img/show00004.png" class="rounded-circle img-fluid border" /></a>
               <h5 class="text-center mt-3 mb-3">Zapatillas de Gym</h5>
               <p class="text-center"><a class="btn btn-primary">Ver tienda</a></p>
             </div>
           </div>
           <div className='class="col-12 col-md-4 p-2 mt-2'>
             <div class="card h-100 p-3">
-              <a href="#"><img src="./assets/img/category_img_02.jpg" class="rounded-circle img-fluid border" /></a>
+              <a href="#"><img src="./assets/products/show00005.png" class="rounded-circle img-fluid border" /></a>
               <h5 class="text-center mt-3 mb-3">Zapatillas para correr</h5>
               <p class="text-center"><a class="btn btn-primary">Ver tienda</a></p>
             </div>
           </div>
           <div className='class="col-12 col-md-4 p-2 mt-2'>
             <div class="card h-100 p-3">
-              <a href="#"><img src="./assets/img/category_img_02.jpg" class="rounded-circle img-fluid border" /></a>
+              <a href="#"><img src="./assets/products/show00001.png" class="rounded-circle img-fluid border" /></a>
               <h5 class="text-center mt-3 mb-3">Zapatillas para senderismo</h5>
               <p class="text-center"><a class="btn btn-primary">Ver tienda</a></p>
             </div>
@@ -146,7 +142,7 @@ const Home = () => {
             <div class="col-12 col-md-4 mb-4">
               <div class="card h-100">
                 <a href="shop-single.html">
-                  <img src="./assets/img/category_img_02.jpg" class="card-img-top" alt="..." />
+                  <img src="./assets/products/show00002.png" class="card-img-top" alt="..." />
                 </a>
                 <div class="card-body">
                   <ul class="list-unstyled d-flex justify-content-between">
@@ -170,7 +166,7 @@ const Home = () => {
             <div class="col-12 col-md-4 mb-4">
               <div class="card h-100">
                 <a href="shop-single.html">
-                  <img src="./assets/img/category_img_02.jpg" class="card-img-top" alt="..." />
+                  <img src="./assets/products/show00006.png" class="card-img-top" alt="..." />
                 </a>
                 <div class="card-body">
                   <ul class="list-unstyled d-flex justify-content-between">
@@ -194,7 +190,7 @@ const Home = () => {
             <div class="col-12 col-md-4 mb-4">
               <div class="card h-100">
                 <a href="shop-single.html">
-                  <img src="./assets/img/category_img_02.jpg" class="card-img-top" alt="..." />
+                  <img src="./assets/products/show00007.png" class="card-img-top" alt="..." />
                 </a>
                 <div class="card-body">
                   <ul class="list-unstyled d-flex justify-content-between">
