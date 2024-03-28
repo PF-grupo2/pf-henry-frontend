29ef9712fc18e5038f7a60fd4663f8448ff4108b
enzojgcaceres <enzojgcaceres@gmail.com>
2024-03-28 14:27:43 -0300

    Fix -Cambio en el indentado en main.jsx

--- FrontEnd-ShoeKingdom/src/main.jsx
+++ FrontEnd-ShoeKingdom/src/main.jsx
@@ -12,9 +12,9 @@ import {Provider} from 'react-redux'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
    <BrowserRouter>
-    <Provider store={store}>
-    <App />
-    </Provider>
-      </BrowserRouter>
+     <Provider store={store}>
+       <App />
+     </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
