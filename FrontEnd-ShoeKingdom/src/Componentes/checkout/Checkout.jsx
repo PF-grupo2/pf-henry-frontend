d3431cc02e3a24815398a23b06178fc869e15a64
enzojgcaceres <enzojgcaceres@gmail.com>
2024-04-14 16:05:15 -0300

    Feixing URL from checkout

--- FrontEnd-ShoeKingdom/src/Componentes/checkout/Checkout.jsx
+++ FrontEnd-ShoeKingdom/src/Componentes/checkout/Checkout.jsx
@@ -240,7 +240,7 @@ function Cheackout() {
 const createPreference = async ()=>{
   console.log(orderData);
   try {
-    const response = await axios.post("http://localhost:3000/api/v1/mercadopago",{
+    const response = await axios.post("https://pf-henry-backend-agsr.onrender.com/mercadopago",{
       items: orderData
     })
     const {id} = response.data
