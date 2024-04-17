69d40a311248190bd72991400d4b6dbd53920732
njmartinez23 <martinezn710@gmail.com>
2024-04-17 14:31:34 -0300

    Boton a /login

--- FrontEnd-ShoeKingdom/src/Componentes/buttons/Login.jsx
+++ FrontEnd-ShoeKingdom/src/Componentes/buttons/Login.jsx
@@ -1,17 +1,16 @@
-import { useAuth0 } from '@auth0/auth0-react';
+import { NavLink } from 'react-router-dom';
 import './Cart.css';
+
 const Login = () => {
-
-    const { loginWithRedirect } = useAuth0();
-
     return (
         <div>
-            <button onClick={() => loginWithRedirect()} type="button" className="ButtonCart" data-bs-toggle="modal" data-bs-target="#loginModal">
-               <span className='fa fa-sign-in me-1'></span>
-            </button>
+            <NavLink to="/login">
+                <button type="button" className="ButtonCart">
+                    <span className='fa fa-sign-in me-1'></span>
+                </button>
+            </NavLink>
         </div>
-       
     )
 }
 
-export default Login;
+export default Login
