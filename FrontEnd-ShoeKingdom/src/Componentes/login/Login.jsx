1644a31d8b05d8df18279b2f4f03f957d00de436
Cristhian Rodríguez <crisrodam1996@gmail.com>
2024-04-17 12:15:02 -0500

    Change Base URL

--- FrontEnd-ShoeKingdom/src/Componentes/login/Login.jsx
+++ FrontEnd-ShoeKingdom/src/Componentes/login/Login.jsx
@@ -1,14 +1,15 @@
-import { useDispatch } from 'react-redux'; // Importa useDispatch para disparar acciones de Redux
-import { useNavigate } from 'react-router-dom';
-import Swal from 'sweetalert2';
-import { setToken } from '../../../Redux/Actions/actions'; // Importa la acción setToken
-import { utilsStorage } from '../utils';
-import { useState } from 'react';
+import { useDispatch } from "react-redux"; // Importa useDispatch para disparar acciones de Redux
+import { useNavigate } from "react-router-dom";
+import Swal from "sweetalert2";
+import { setToken } from "../../../Redux/Actions/actions"; // Importa la acción setToken
+import { utilsStorage } from "../utils";
+import { useState } from "react";
+import { BASE_URL } from "../../config";
 
-function LoginForm() { 
+function LoginForm() {
   const [formData, setFormData] = useState({
-    mail: '',
-    password: ''
+    mail: "",
+    password: "",
   });
 
   const dispatch = useDispatch(); // Obtiene la función dispatch
@@ -16,23 +17,21 @@ function LoginForm() {
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFormData(prevState => ({
+    setFormData((prevState) => ({
       ...prevState,
-      [name]: value
+      [name]: value,
     }));
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      
-      const response = await fetch('http://localhost:3000/api/v1/auth/login/', {
-
-        method: 'POST',
+      const response = await fetch(`${BASE_URL}/auth/login/`, {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json'
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(formData)
+        body: JSON.stringify(formData),
       });
 
       if (response.ok) {
@@ -40,50 +39,49 @@ function LoginForm() {
         const { user, token } = data;
 
         // Guardar el token en localStorage
-        
-        utilsStorage.saveDataStorage('token', token)
 
-        console.log('Token almacenado en localStorage:', token);
-        
+        utilsStorage.saveDataStorage("token", token);
+
+        console.log("Token almacenado en localStorage:", token);
+
         // Guardar el token en el estado global de Redux
         dispatch(setToken(token));
-        
 
-        console.log('Token almacenado en estado global de Redux:', token)
+        console.log("Token almacenado en estado global de Redux:", token);
 
         const loggedInUser = {
           id: user.id,
           name: user.name,
           email: user.mail,
           phone: user.phone,
-          isAdmin: user.isAdmin
+          isAdmin: user.isAdmin,
         };
-        console.log("esto es lo que llega al log", loggedInUser)
-        
-        utilsStorage.saveDataStorage("userSession", loggedInUser)
-        
+        console.log("esto es lo que llega al log", loggedInUser);
 
-        navigate('/');
+        utilsStorage.saveDataStorage("userSession", loggedInUser);
+
+        navigate("/");
 
         Swal.fire({
-          icon: 'success',
-          title: 'Inicio de sesión exitoso',
-          text: '¡Bienvenido de vuelta!'
+          icon: "success",
+          title: "Inicio de sesión exitoso",
+          text: "¡Bienvenido de vuelta!",
         });
       } else {
         const errorMessage = await response.json();
         Swal.fire({
-          icon: 'error',
-          title: 'Error al iniciar sesión',
-          text: errorMessage.message || 'Por favor, inténtelo de nuevo más tarde.'
+          icon: "error",
+          title: "Error al iniciar sesión",
+          text:
+            errorMessage.message || "Por favor, inténtelo de nuevo más tarde.",
         });
       }
     } catch (error) {
-      console.error('Error de red:', error);
+      console.error("Error de red:", error);
       Swal.fire({
-        icon: 'error',
-        title: 'Error de red',
-        text: 'Ocurrió un error de red. Por favor, inténtelo de nuevo más tarde.'
+        icon: "error",
+        title: "Error de red",
+        text: "Ocurrió un error de red. Por favor, inténtelo de nuevo más tarde.",
       });
     }
   };
@@ -91,10 +89,29 @@ function LoginForm() {
   return (
     <form onSubmit={handleSubmit}>
       <h2>Iniciar Sesión</h2>
-      <label htmlFor="loginMail">Correo electrónico:</label><br />
-      <input type="email" id="loginMail" name="mail" value={formData.mail} onChange={handleChange} required /><br />
-      <label htmlFor="loginPassword">Contraseña:</label><br />
-      <input type="password" id="loginPassword" name="password" value={formData.password} onChange={handleChange} required /><br /><br />
+      <label htmlFor="loginMail">Correo electrónico:</label>
+      <br />
+      <input
+        type="email"
+        id="loginMail"
+        name="mail"
+        value={formData.mail}
+        onChange={handleChange}
+        required
+      />
+      <br />
+      <label htmlFor="loginPassword">Contraseña:</label>
+      <br />
+      <input
+        type="password"
+        id="loginPassword"
+        name="password"
+        value={formData.password}
+        onChange={handleChange}
+        required
+      />
+      <br />
+      <br />
       <button type="submit">Iniciar Sesión</button>
     </form>
   );
